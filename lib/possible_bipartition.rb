133f7e2c44daf677e3e0e1edaf91867f794dcdc1
C Gutierrez <gutierrez.clara.es@gmail.com>
2020-04-13 22:22:45 -0700

    some functionality

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -38,3 +38,32 @@ def possible_bipartition(set_array)
   return true
   
 end
+
+## NOTE: this method does not work for the test on LN 52. I considered alternating the rooms between which the elements are placed, which would work as a band-aid but not solve the problem if the test set were to change. 
+
+## Below is another attempt to make the decision-making process an algorithm and it became even more messy. 
+
+
+# def possible_bipartition(set_array)
+#   if set_array == []
+#     return true
+#   end
+
+#   i = 0
+
+#   reference_hash = {}
+
+#   until i == set_array.length do
+#     reference_hash[i] = set_array[i]
+#     i += 1
+#   end
+
+#   sorted = reference_hash.sort_by {|identity, dislikes| dislikes}
+
+#   puts "#{sorted}"
+
+#   sorted_too = sorted.sort_by{|groups| groups[1].length}.reverse
+
+#   puts "#{sorted_too}"
+
+# end
