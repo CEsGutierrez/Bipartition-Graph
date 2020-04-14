1303dcaafbe379190511cc4b2c1bc198e70db186
C Gutierrez <gutierrez.clara.es@gmail.com>
2020-04-13 21:12:11 -0700

    some functionality

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,40 @@
 
-def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+def possible_bipartition(set_array)
+  
+  if set_array == []
+    return true
+  end
+  
+  room_a = []
+  room_b = []
+  
+  room_charming = []
+  
+  length = set_array.length
+  
+  i = 0
+  
+  until i == length do
+    if set_array[i] == []
+      room_charming.push(i)
+      i += 1
+    else
+      set_array[i].each do |dislike|
+        if !room_a.include?(i)
+          room_a.push(dislike)
+        elsif room_b.include?(i)
+          return false
+        else
+          room_b.push(dislike)
+        end
+      end
+      i += 1
+    end
+  end
+  
+  room_a.push(room_charming)
+  room_a.flatten!
+  
+  return true
+  
 end
